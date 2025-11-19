4c28c0fd45c2e15f6316c658d94664211d4208f0
TusharSin810 <tsinghal666@gmail.com>
2025-11-19 19:44:57 +0530

    Added Dummy Routes

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,8 +1,18 @@
 import express from 'express'
 import cors from 'cors'
+import { prismaClient } from "db/client";
+import { authMiddleware } from './middleware';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.post("/signin", (req,res) => {
+
+});
+
+app.get("/calender", authMiddleware, (req,res) => {
+
+});
 
 app.use(cors());
 
