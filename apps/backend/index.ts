22fc14fbe89c955d29e448112fb1f6e6d73769d7
TusharSin810 <tsinghal666@gmail.com>
2025-11-19 20:36:36 +0530

    Middleware Implemented

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -2,19 +2,57 @@ import express from 'express'
 import cors from 'cors'
 import { prismaClient } from "db/client";
 import { authMiddleware } from './middleware';
+import jwt from 'jsonwebtoken';
+import { SignupSchema } from "common/inputs";
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
 
-app.post("/signin", (req,res) => {
+app.post("/signin", async (req,res) => {
+    const {success, data} = SignupSchema.safeParse(req.body);
+    if(!success){
+        res.status(403).json({
+            message: "Incorrect Credentials"
+        })
+        return;
+    }
+
+    const email = data.email;
+    const password = data.password;
+
+    const user = await prismaClient.user.findFirst({
+        where:{
+            email : email
+        }
+    })
+    if(!user){
+        res.status(403).json({
+            message: "User Does Not Exist"
+        })
+        return;
+    }
+    if(user.password !== password){
+        res.status(403).json({
+            message: "Incorrect Password"
+        })
+    }
+
+    const token = jwt.sign({
+        userId: user.id
+    },process.env.JWT_SECRET!);
+
+    res.json({
+        token,
+        message: "Signed In Successfully"
+    })
 
 });
 
 app.get("/calender", authMiddleware, (req,res) => {
 
 });
-
-app.use(cors());
 
 app.listen(port , () => {
     console.log(`Listening On Port : ${port}`);
--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,3 +1,3 @@
 import { PrismaClient } from "./generated/prisma/client";
 
-export const prismClient = new PrismaClient();
+export const prismaClient = new PrismaClient();
